11cea43cec201a00add3ff15be7dc0ef374f0dfd
Dmitry Shulyak <dshulyak@mirantis.com>
2013-07-01 13:15:25 +0300

    install test dependencies in subprocess

--- setup.py
+++ setup.py
@@ -1,57 +1,57 @@
 import multiprocessing
+import subprocess
 import setuptools
 
 
 def load_requirements(requirements_path):
     with open(requirements_path, 'r') as reqs:
-
         return reqs.read().split('\n')
 
+def run_setup():
+    subprocess.call(['pip', 'install'] + load_requirements('tools/commands-requires'))
+    setuptools.setup(
 
-setuptools.setup(
+        name='testing_adapter',
+        version='0.2',
 
-    name='testing_adapter',
-    version='0.2',
+        description='cloud computing testing',
 
-    description='cloud computing testing',
+        zip_safe = False,
 
-    zip_safe = False,
+        test_suite = 'tests',
 
-    test_suite = 'tests',
-
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Framework :: Setuptools Plugin',
-        'Environment :: OpenStack',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: System :: Testing',
-    ],
-
-    packages=setuptools.find_packages(exclude=['tests', 'bin']),
-
-    include_package_data=True,
-
-    install_requires=load_requirements('tools/pip-requires'),
-
-    tests_require=load_requirements('tools/test-requires'),
-
-    # dependency_links = [
-    #     'https://github.com/Mirantis/fuel-ostf-tests.git@feature/package#egg=ostf-tests==0.1'
-    # ],
-
-    entry_points={
-        'plugins': [
-            'nose = ostf_adapter.transport.nose_adapter:NoseDriver'
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Framework :: Setuptools Plugin',
+            'Environment :: OpenStack',
+            'Intended Audience :: Information Technology',
+            'Intended Audience :: System Administrators',
+            'License :: OSI Approved :: Apache Software License',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: System :: Testing',
         ],
-        'console_scripts': [
-            'ostf-server = bin.adapter_api:main',
-            'ostf-db = bin.adapter_db:main'
-        ]
-    },
-)
+
+        packages=setuptools.find_packages(exclude=['tests', 'bin']),
+
+        include_package_data=True,
+
+        install_requires=load_requirements('tools/pip-requires'),
+
+        tests_require=load_requirements('tools/test-requires'),
+
+        entry_points={
+            'plugins': [
+                'nose = ostf_adapter.transport.nose_adapter:NoseDriver'
+            ],
+            'console_scripts': [
+                'ostf-server = bin.adapter_api:main',
+                'ostf-db = bin.adapter_db:main'
+            ]
+        },
+    )
+
+if __name__ == '__main__':
+    run_setup()
