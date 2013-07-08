8b5efcf9186defaf376de29d1b0367941c8438c8
Dmitry Shulyak <dshulyak@mirantis.com>
2013-07-08 12:31:49 +0300

    problem with alembic config

--- ostf_adapter/storage/__init__.py
+++ ostf_adapter/storage/__init__.py
@@ -8,5 +8,5 @@ log = logging.getLogger(__name__)
 DEFAULT_DBPATH = 'postgresql+psycopg2://adapter:demo@localhost/testing_adapter'
 
 def get_storage(dbpath=None):
-    log.info('GET STORAGE FOR - %s' % conf.get('db_path', '') or DEFAULT_DBPATH)
-    return sql_storage.SqlStorage(conf.get('db_path', '') or DEFAULT_DBPATH)
+    log.info('GET STORAGE FOR - %s' % conf.get('dbpath', '') or DEFAULT_DBPATH)
+    return sql_storage.SqlStorage(conf.get('dbpath', '') or DEFAULT_DBPATH)
--- ostf_adapter/storage/sql/cli.py
+++ ostf_adapter/storage/sql/cli.py
@@ -3,10 +3,11 @@ import sys
 from alembic import command, config, util
 from pecan import conf
 
+
 def do_apply_migrations():
-    conf = config.Config(
+    alembic_conf = config.Config(
         os.path.join(os.path.dirname(__file__), 'alembic.ini')
     )
-    conf.set_main_option('script_location', 'ostf_adapter.storage.sql:migrations')
-    conf.set_main_option('sqlalchemy.url', conf.dbpath)
-    getattr(command, 'upgrade')(conf, 'head')
+    alembic_conf.set_main_option('script_location', 'ostf_adapter.storage.sql:migrations')
+    alembic_conf.set_main_option('sqlalchemy.url', conf.dbpath)
+    getattr(command, 'upgrade')(alembic_conf, 'head')
