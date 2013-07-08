6601819c2529f0e7c093931d8d10388b55c5e9bc
ekonstantinov <ekonstantinov@mirantis.com>
2013-07-08 15:30:46 +0300

    adding tests for the new API adding new REST client adding post-install commands patching

--- utils/update_commands.py
+++ utils/update_commands.py
@@ -3,16 +3,20 @@ from json import loads, dumps
 
 
 def main():
-    with open('data/commands.json', 'rw+') as file:
-        data = loads(file.read())
+    with open('data/commands.json', 'rw+') as commands:
+        data = loads(commands.read())
         for item in data:
-            data[item]['argv'].append("--with-xunit")
+            if data[item].get('argv'):
+                if "--with-xunit" not in data[item]['argv']:
+                    data[item]['argv'].append("--with-xunit")
+            else:
+                data[item]['argv'] = ["--with-xunit", ]
         test_apps = {"plugin-general": {"test_path": "tests/functional/dummy_tests/general_test.py", "driver": "nose"},
                      "plugin-stopped": {"test_path": "tests/functional/dummy_tests/stopped_test.py", "driver": "nose"}}
         data.update(test_apps)
-        file.seek(0)
-        file.write(dumps(data))
-        file.truncate()
+        commands.seek(0)
+        commands.write(dumps(data))
+        commands.truncate()
 
 if __name__ == '__main__':
     main()
