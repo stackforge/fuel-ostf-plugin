c4b2904849fc959dca55d8a0e24c6bdd47e81299
Dmitry Shulyak <dshulyak@mirantis.com>
2013-07-23 17:35:26 +0300

    return 0 if there was no exceptions, 1 if was

--- ostf_adapter/storage/sql/cli.py
+++ ostf_adapter/storage/sql/cli.py
@@ -15,12 +15,19 @@
 import os
 from alembic import command, config
 from pecan import conf
+import logging
 
+log = logging.getLogger(__name__)
 
 def do_apply_migrations():
-    alembic_conf = config.Config(
-        os.path.join(os.path.dirname(__file__), 'alembic.ini')
-    )
-    alembic_conf.set_main_option('script_location', 'ostf_adapter.storage.sql:migrations')
-    alembic_conf.set_main_option('sqlalchemy.url', conf.dbpath)
-    getattr(command, 'upgrade')(alembic_conf, 'head')
+    try:
+        alembic_conf = config.Config(
+            os.path.join(os.path.dirname(__file__), 'alembic.ini')
+        )
+        alembic_conf.set_main_option('script_location', 'ostf_adapter.storage.sql:migrations')
+        alembic_conf.set_main_option('sqlalchemy.url', conf.dbpath)
+        getattr(command, 'upgrade')(alembic_conf, 'head')
+        return 0
+    except Exception, e:
+        log.exception('Migration failed')
+        return 1
